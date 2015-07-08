9bd1ccf724376b922896667df2e705ebc0e6b996
ne-luboff <ne-luboff@yandex.ua>
2015-07-08 18:38:52 +0300

    Enable selling without fb

--- api/items/handlers.py
+++ api/items/handlers.py
@@ -5,7 +5,7 @@ import datetime
 import random
 from sqlalchemy import or_, desc, and_, func
 from api.items.models import Item, ItemPhoto
-from api.tags.models import Tag
+from api.tags.models import Tag, Platform, Category, Subcategory, Color, Condition
 from base import ApiHandler, die, paginate
 from helpers import route, sa_object_to_dict
 from utility.google_api import get_city_by_code
@@ -133,8 +133,8 @@ class ItemsHandler(ApiHandler):
             die(401)
 
         # check selling ability
-        if not self.user.facebook_id:
-            return self.make_error("Sorry, but you can't sale anything 'cause you don't link your FB account")
+        # if not self.user.facebook_id:
+        #     return self.make_error("Sorry, but you can't sale anything 'cause you don't link your FB account")
         if not self.user.email_status:
             return self.make_error("Sorry, but you can't sale anything 'cause you don't confirm your email address")
 
@@ -384,3 +384,352 @@ class PostCodeHandler(ApiHandler):
         if error:
             return self.make_error(error)
         return self.success({'city': data})
+
+
+# TODO for listing edition
+@route('listings')
+class ListingHandler(ApiHandler):
+    allowed_methods = ('GET', 'POST', )
+
+    def read(self):
+
+        if not self.user:
+            die(401)
+
+        item_id = self.get_arg('item_id', int)
+        q = self.get_arg('q', str)
+        response = dict()
+
+        # get item by id
+        if item_id:
+            # first get this item
+            item = self.session.query(Item).filter(Item.id == item_id).first()
+            if not item:
+                return self.make_error('No item with id %s' % item_id)
+
+            # find 6 items with similar category | platform | subcategory
+            similar_items = self.session.query(Item).filter(and_(or_(Item.platform_id == item.platform_id,
+                                                                     Item.category_id == item.category_id,
+                                                                     Item.subcategory_id == item.subcategory_id),
+                                                                 Item.id != item.id)).limit(6)
+            # find 6 items of this user
+            user_items = self.session.query(Item).filter(and_(Item.user_id == item.user_id,
+                                                              Item.id != item.id)).limit(6)
+            current_item_response = item.item_response
+            current_item_response['user'] = item.user.user_response
+            response['item'] = current_item_response
+            response['similar_items'] = [i.item_response for i in similar_items]
+            response['user_items'] = [i.item_response for i in user_items]
+        # else return all items
+        else:
+            # TODO uncomment to return items by user interests
+            # user_tags_set = interested_user_tag_ids(self)
+            # item_ids = interested_user_item_ids(self, user_tags_set)
+            # items = self.session.query(Item).filter(Item.id.in_(list(item_ids))).order_by(desc(Item.id))
+
+            # TODO 2015-07-08 return all items
+            items = self.session.query(Item).order_by(desc(Item.id))
+
+            # search
+            # if we have searching query we must filtered all items
+            if q:
+                # first we must get all items
+                all_items = self.session.query(Item)
+
+                # next get all tags names to search.
+                # note! we can search only by platform name, category name and subcategory name
+                tag_names_to_search = set()
+                for i in items:
+                    tag_names_to_search.add(i.platform.name)
+                    tag_names_to_search.add(i.category.name)
+                    tag_names_to_search.add(i.subcategory.name)
+
+                # this is set with suitable tag names
+                right_tag_names = set()
+                right_title_or_description_item_ids = set()
+
+                # we search by every word in phrase
+                # so separate query string by whitespace symbol
+                q_list = q.split(' ')
+
+                # go through every query word and search it in tags and items titles/descriptions
+                for q_word in q_list:
+
+                    # search in every tag key by every query word
+                    for key in tag_names_to_search:
+                        if q_word in key:
+                            right_tag_names.add(key)
+
+                    # filtered items and get items with fit description or title
+                    right_title_or_description = [i.id for i in all_items.filter(or_(Item.title.ilike(u'%{0}%'.format(q_word)),
+                                                                                     Item.description.ilike(u'%{0}%'.format(q_word))))]
+                    if right_title_or_description:
+                        for i in right_title_or_description:
+                            right_title_or_description_item_ids.add(i)
+
+                # next step - get right tags ids
+                right_tag_ids = [t.id for t in self.session.query(Tag).filter(func.lower(Tag.name).in_(right_tag_names))]
+
+                # select items by suitable tag name
+                right_tag_item_ids = [i.id for i in all_items.filter(or_(Item.platform_id.in_(right_tag_ids),
+                                                                         Item.category_id.in_(right_tag_ids),
+                                                                         Item.subcategory_id.in_(right_tag_ids)))]
+
+                # finally get all items which match search terms
+                items = all_items.filter(Item.id.in_(list(set(right_tag_item_ids +
+                                                              list(right_title_or_description_item_ids))))).order_by(desc(Item.id))
+
+
+                # TODO optimize using this
+                # filtered them by platform | category | subcategory | title | condition
+                # items = all_items.filter(or_(Item.platform.name.ilike(u'%{0}%'.format(q)),
+                #                              Item.category.name.ilike(u'%{0}%'.format(q)),
+                #                              Item.subcategory.name.ilike(u'%{0}%'.format(q)),
+                #                              Item.title.ilike(u'%{0}%'.format(q)),
+                #                              Item.description.ilike(u'%{0}%'.format(q))))
+            # pagination
+            page = self.get_arg('p', int, 1)
+            page_size = self.get_arg('page_size', int, 100)
+            paginator, items = paginate(items, page, page_size)
+            if paginator['pages'] < page:
+                items = []
+            response['paginator'] = paginator
+            response['items'] = [i.item_response for i in items]
+
+        return self.success(response)
+
+    def create(self):
+
+        if self.user is None:
+            die(401)
+
+        # check selling ability
+        if not self.user.facebook_id:
+            return self.make_error("Sorry, but you can't sale anything 'cause you don't link your FB account")
+        if not self.user.email_status:
+            return self.make_error("Sorry, but you can't sale anything 'cause you don't confirm your email address")
+
+        logger.debug('REQUEST_OBJECT_NEW_ITEM')
+        logger.debug(self.request_object)
+
+        title = ''
+        description = ''
+        platform_id = ''
+        category_id = ''
+        subcategory_id = ''
+        condition_id = ''
+        color_id = ''
+        retail_price = ''
+        selling_price = ''
+        shipping_price = ''
+        collection_only = ''
+        barcode = ''
+        photos = ''
+        post_code = ''
+        city = ''
+
+        empty_field_error = []
+
+        if self.request_object:
+            if 'title' in self.request_object:
+                title = self.request_object['title']
+
+            if 'description' in self.request_object:
+                description = self.request_object['description']
+
+            if 'platform' in self.request_object:
+                platform_id = self.request_object['platform']
+
+            if 'category' in self.request_object:
+                category_id = self.request_object['category']
+
+            if 'subcategory' in self.request_object:
+                subcategory_id = self.request_object['subcategory']
+
+            if 'condition' in self.request_object:
+                condition_id = self.request_object['condition']
+
+            if 'color' in self.request_object:
+                color_id = self.request_object['color']
+
+            if 'retail_price' in self.request_object:
+                retail_price = float(self.request_object['retail_price'])
+
+            if 'selling_price' in self.request_object:
+                selling_price = float(self.request_object['selling_price'])
+
+            if 'shipping_price' in self.request_object:
+                shipping_price = float(self.request_object['shipping_price'])
+
+            if 'collection_only' in self.request_object:
+                collection_only = self.request_object['collection_only']
+
+            if 'barcode' in self.request_object:
+                barcode = self.request_object['barcode']
+
+            if 'photos' in self.request_object:
+                photos = self.request_object['photos']
+
+            if 'post_code' in self.request_object:
+                post_code = self.request_object['post_code']
+
+            if 'city' in self.request_object:
+                city = self.request_object['city']
+
+        if not title:
+            empty_field_error.append('title')
+
+        if not description:
+            empty_field_error.append('description')
+
+        if not platform_id:
+            empty_field_error.append('platform')
+
+        if not category_id:
+            empty_field_error.append('category')
+
+        if not subcategory_id:
+            empty_field_error.append('subcategory')
+
+        if not condition_id:
+            empty_field_error.append('condition')
+
+        if not color_id:
+            empty_field_error.append('color')
+
+        if not retail_price:
+            empty_field_error.append('retail price')
+
+        if not selling_price:
+            empty_field_error.append('selling price')
+
+        if not shipping_price:
+            empty_field_error.append('shipping price')
+
+        # if not collection_only:
+        #     empty_field_error.append('collection only flag')
+
+        if not photos:
+            empty_field_error.append('photos')
+
+        for photo in photos:
+            if not photo:
+                empty_field_error.append('photos')
+                break
+
+        if not post_code:
+            empty_field_error.append('post code')
+
+        if not city:
+            empty_field_error.append('city')
+
+        if empty_field_error:
+            empty_fields = ','.join(empty_field_error)
+            return {
+                'status': 6,
+                'message': 'You must select a %s in order to create a listing.' % empty_fields,
+                'empty_fields': empty_fields
+            }
+
+        # first check all tags
+        # check platforms
+        platform = self.session.query(Platform).filter(Platform.id == platform_id).first()
+        if not platform:
+            return self.make_error('No platform with id %s' % platform_id)
+
+        # check category
+        category = self.session.query(Category).filter(Category.id == category_id).first()
+        if not category:
+            return self.make_error('No category with id %s' % category_id)
+
+        # check subcategory
+        subcategory = self.session.query(Subcategory).filter(Subcategory.id == subcategory_id).first()
+        if not subcategory:
+            return self.make_error('No subcategory with id %s' % category_id)
+
+        # check color
+        colour = self.session.query(Color).filter(Color.id == color_id).first()
+        if not colour:
+            return self.make_error('No color with id %s' % color_id)
+
+        # check condition
+        condition = self.session.query(Condition).filter(Condition.id == condition_id).first()
+        if not condition:
+            return self.make_error('No condition with id %s' % condition_id)
+
+        # check is this nesting right
+        platform_categories = [c.id for c in platform.platform_category]
+        if category_id not in platform_categories:
+            return self.make_error("Platform tag %s hasn't category %s. Try again" % (platform.title.upper(),
+                                                                                      category.title.upper()))
+        category_subcategories = [c.id for c in category.category_subcategories]
+        if subcategory_id not in category_subcategories:
+            return self.make_error("Category tag %s (%s) hasn't subcategory %s. Try again" % (category.title.upper(),
+                                                                                              category.platform.title(),
+                                                                                              subcategory.title.upper()))
+
+        subcategory_color = [c.id for c in subcategory.subcategory_colour]
+        if color_id not in subcategory_color:
+            return self.make_error("Subcategory tag %s (%s -> %s) hasn't color %s. Try again"
+                                   % (subcategory.title.upper(), subcategory.category.title.upper(),
+                                                                                              category.platform.title(),
+                                                                                              subcategory.title.upper()))
+
+        # secondary check other fields
+        # price handler
+        if retail_price < 1:
+            return self.make_error(u'Retail price must be greater than £1')
+
+        if selling_price > retail_price:
+                return self.make_error("Retail price must be greater than selling price")
+
+        if len(photos) > 3:
+            return self.make_error('You can add only 3 photos')
+
+        # finally create item
+        item = Item()
+        item.user = self.user
+        item.created_at = datetime.datetime.utcnow()
+        item.updated_at = datetime.datetime.utcnow()
+        item.title = title
+        item.description = description
+
+        if barcode:
+            item.barcode = barcode
+
+        item.platform_id = platform_id
+        item.category_id = category_id
+        item.subcategory_id = subcategory_id
+        item.condition_id = condition_id
+        item.color_id = color_id
+        item.retail_price = retail_price
+        item.selling_price = selling_price
+
+        if selling_price != retail_price:
+            # calculate discount value
+            discount = int(round((retail_price - selling_price) / retail_price * 100))
+            item.discount = discount
+
+        item.shipping_price = shipping_price
+        if collection_only:
+            item.collection_only = True
+        else:
+            item.collection_only = False
+
+        item.post_code = post_code
+        item.city = city
+
+        # self.session.flush(item)
+        # self.session.commit()
+
+        # photos
+        for photo in photos:
+            item_photo = ItemPhoto()
+            item_photo.created_at = datetime.datetime.utcnow()
+            item_photo.item = item
+            item_photo.image_url = photo
+            self.session.add(item_photo)
+            self.session.commit()
+
+        self.session.commit()
+        return self.success({'item': item.item_response})
--- api/items/models.py
+++ api/items/models.py
@@ -1,8 +1,7 @@
 import datetime
-from sqlalchemy import Column, DateTime, ForeignKey, String, Enum, Boolean, Numeric
+from sqlalchemy import Column, DateTime, ForeignKey, String, Boolean, Numeric
 from sqlalchemy import Integer
 from sqlalchemy.orm import relationship, backref
-from api.tags.models import Tag
 from api.users.models import User
 from orm import Base
 
@@ -95,5 +94,98 @@ class ItemPhoto(Base):
     item_id = Column(Integer, ForeignKey('items.id'), nullable=False, index=True)
     item = relationship(Item, backref=backref('item_photos', order_by=id, cascade="all,delete", lazy='dynamic'),
                         foreign_keys=item_id)
+
+    image_url = Column(String, nullable=False)
+
+
+# TODO  new release
+
+
+class Listing(Base):
+    __tablename__ = 'listings'
+
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
+
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False, index=True)
+    user = relationship('User', backref=backref('listings', order_by=id, cascade="all,delete", lazy='dynamic'),
+                        foreign_keys=user_id)
+
+    title = Column(String, nullable=False, default='')
+    description = Column(String, nullable=False, default='')
+    barcode = Column(String, nullable=False, default='')
+
+    # listing details (tags)
+    platform_id = Column(Integer, ForeignKey('platforms.id'), nullable=False, index=True)
+    platform = relationship('Platform', backref=backref('platform_items', order_by=id, cascade="all,delete", lazy='dynamic'),
+                            foreign_keys=platform_id)
+
+    category_id = Column(Integer, ForeignKey('categories.id'), nullable=False, index=True)
+    category = relationship('Category', backref=backref('category_items', order_by=id, cascade="all,delete", lazy='dynamic'),
+                            foreign_keys=category_id)
+
+    subcategory_id = Column(Integer, ForeignKey('subcategories.id'), nullable=False, index=True)
+    subcategory = relationship('Subcategory', backref=backref('subcategory_items', order_by=id, cascade="all,delete",
+                                                              lazy='dynamic'), foreign_keys=subcategory_id)
+
+    condition_id = Column(Integer, ForeignKey('conditions.id'), nullable=False, index=True)
+    condition = relationship('Condition', backref=backref('condition_items', order_by=id, cascade="all,delete",
+                                                          lazy='dynamic'), foreign_keys=condition_id)
+
+    color_id = Column(Integer, ForeignKey('colors.id'), nullable=False, index=True)
+    color = relationship('Color', backref=backref('color_items', order_by=id, cascade="all,delete", lazy='dynamic'),
+                         foreign_keys=color_id)
+
+    # price
+    retail_price = Column(Numeric, nullable=False)
+    selling_price = Column(Numeric, nullable=True)
+    discount = Column(Integer, nullable=True)
+
+    shipping_price = Column(Numeric, nullable=True)
+    collection_only = Column(Boolean, nullable=False, default=False)
+
+    # location info info
+    post_code = Column(String, nullable=True)
+    city = Column(String, nullable=True)
+    location_lat = Column(Numeric, nullable=True)
+    location_lon = Column(Numeric, nullable=True)
+
+    def __repr__(self):
+        return '<Item %s (%s)>' % (self.id, self.title)
+
+    @property
+    def response(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'created_at': self.created_at,
+            'title': self.title,
+            'description': self.description,
+            'platform': self.platform_id,
+            'category': self.category_id,
+            'subcategory': self.subcategory_id,
+            'condition': self.condition_id,
+            'color': self.color_id,
+            'retail_price': self.retail_price,
+            'selling_price': self.selling_price,
+            'discount': self.discount,
+            'shipping_price': self.shipping_price,
+            'collection_only': self.collection_only,
+            'post_code': self.post_code,
+            'city': self.city,
+            'photos': [photo.image_url for photo in self.listing_photos],
+        }
+
+
+class ListingPhoto(Base):
+    __tablename__ = 'listing_photos'
+
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
+
+    listing_id = Column(Integer, ForeignKey('listings.id'), nullable=False, index=True)
+    listing = relationship(Listing, backref=backref('listing_photos', order_by=id, cascade="all,delete", lazy='dynamic'),
+                           foreign_keys=listing_id)
 
     image_url = Column(String, nullable=False)

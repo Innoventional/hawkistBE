1f540e1f2047a9763a88a6fbce6115c8eeacdf1b
Jonathan Erwin <jkeconsultancy@gmail.com>
2015-07-14 11:51:36 +0100

    Update google_api_errors.py
    added full stops

--- ui_messages/errors/utility_errors/google_api_errors.py
+++ ui_messages/errors/utility_errors/google_api_errors.py
@@ -1,4 +1,4 @@
 __author__ = 'ne_luboff'
 
-POST_CODE_DOES_NOT_EXISTS = 'Unable to match your Post Code with a City or Location. Please check it and try again'
-POST_CODE_DOES_NOT_EXISTS_IN_GB = 'Please check the Post Code to ensure that it is correctly formatted'
+POST_CODE_DOES_NOT_EXISTS = 'Unable to match your Post Code with a City or Location. Please check it and try again.'
+POST_CODE_DOES_NOT_EXISTS_IN_GB = 'Please check the Post Code to ensure that it is correctly formatted.'

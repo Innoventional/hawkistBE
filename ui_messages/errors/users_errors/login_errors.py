8da5cd144a1859ba894a75403b7fe147f3368344
Jonathan Erwin <jkeconsultancy@gmail.com>
2015-07-14 11:11:36 +0100

    Update login_errors.py

--- ui_messages/errors/users_errors/login_errors.py
+++ ui_messages/errors/users_errors/login_errors.py
@@ -4,13 +4,13 @@ __author__ = 'ne_luboff'
 Sign up
 """
 # empty phone and facebook token
-SIGN_UP_EMPTY_AUTHORIZATION_DATA = 'Empty authorization data'
+SIGN_UP_EMPTY_AUTHORIZATION_DATA = 'Empty authorisation data'
 
 """
 Log in (in case if user sign up using phone number)
 """
 # empty phone or pin
-LOG_IN_EMPTY_AUTHORIZATION_DATA = 'You must input a mobile number and a pin to sign in.'
+LOG_IN_EMPTY_AUTHORIZATION_DATA = 'You must input a mobile number and a pin code to sign in.'
 
 # if user enter phone number of unexisting user
 LOG_IN_USER_NOT_FOUND = 'There is no user with mobile number %s. Please check the mobile number or sign up.'

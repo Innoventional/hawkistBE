e282b7c4394d08f38ef7bf754806fc109bee737c
Jonathan Erwin <jkeconsultancy@gmail.com>
2015-07-14 11:50:24 +0100

    Update format_verification_errors.py

--- ui_messages/errors/utility_errors/format_verification_errors.py
+++ ui_messages/errors/utility_errors/format_verification_errors.py
@@ -3,18 +3,17 @@ __author__ = 'ne_luboff'
 """
 Phone number verification
 """
-PHONE_VERIFICATION_NOT_DIGITS_ONLY = 'Invalid number format. Digits only'
-PHONE_VERIFICATION_TOO_SHORT_NUMBER = 'Phone number must contain at least 10 digits (country code, operator code and ' \
-                                      'phone number)'
-PHONE_VERIFICATION_TOO_LONG_NUMBER = 'Phone number too long. Maximum length - 15 digits'
+PHONE_VERIFICATION_NOT_DIGITS_ONLY = 'Mobile numbers must consist of numbers only.'
+PHONE_VERIFICATION_TOO_SHORT_NUMBER = 'Mobile numbers must be at least 10 digits long.'
+PHONE_VERIFICATION_TOO_LONG_NUMBER = 'Mobile numbers must have less than 15 digits.'
 
 """
 Email address verification
 """
-EMAIL_VERIFICATION_INVALID_FORMAT = 'Invalid email format. Example: your@mail.com'
+EMAIL_VERIFICATION_INVALID_FORMAT = 'Email addresses must be formatted as email@domain.com'
 
 """
 Username verification
 """
-USERNAME_VERIFICATION_TOO_LONG_USERNAME = 'Too long username. Max length - 50 symbols'
-USERNAME_VERIFICATION_INVALID_FORMAT = 'Username can consist of string, digits, dash symbols and dots'
+USERNAME_VERIFICATION_TOO_LONG_USERNAME = 'Usernames cannot be longer than 50 characters.'
+USERNAME_VERIFICATION_INVALID_FORMAT = 'Usernames cannot contain special characters, only letters and numbers.'

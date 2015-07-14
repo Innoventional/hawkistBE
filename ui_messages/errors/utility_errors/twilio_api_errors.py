6ec576df2998e5d66562382bab81df03ed6af460
ne-luboff <ne-luboff@yandex.ua>
2015-07-14 15:04:34 +0300

    fix syntax error

--- ui_messages/errors/utility_errors/twilio_api_errors.py
+++ ui_messages/errors/utility_errors/twilio_api_errors.py
@@ -1,9 +1,9 @@
 __author__ = 'ne_luboff'
 
-TWILIO_INVALID_PHONE_NUMBER = 'Please input a mobile number starting in '07'. Or else Sign Up with a Facebook account.'
+TWILIO_INVALID_PHONE_NUMBER = "Please input a mobile number starting in '07'. Or else Sign Up with a Facebook account."
 TWILIO_UNSUPPORTED_REGION = 'Permission to send SMS has not been enabled for your region.'
 
 """
 Check number of sent sms last hour
 """
-TWILIO_RICH_SMS_LIMIT = Too many SMS sent. Try again later.'
+TWILIO_RICH_SMS_LIMIT = 'Too many SMS sent. Try again later.'
--- ui_messages/messages/email.py
+++ ui_messages/messages/email.py
@@ -7,10 +7,11 @@ CONFIRM_EMAIL_LETTER_TEXT = 'Welcome to Hawkist!\n' \
 
 # change user group
 ADMIN_BACK_USER_TO_STANDARD_USERTYPE_LETTER_TEXT = 'Looks like you were excluded from Hawkist %s user group.'
-ADMIN_CHANGE_USERTYPE_LETTER_TEXT = 'Congrats!\nYou were added to Hawkist %s user group. Go to %s and use your email address ' \
-                             'and this temporary password to log in administration module:\n%s\nEnjoy!'
+ADMIN_CHANGE_USERTYPE_LETTER_TEXT = 'Congrats!\nYou were added to Hawkist %s user group. Go to %s and use your email ' \
+                                    'address and this temporary password to log in administration module:\n%s\nEnjoy!'
 ADMIN_CHANGE_USERTYPE_LETTER_SUBJECT = 'Permissions changed'
 
 # change user phone number
-ADMIN_PHONE_NUMBER_CHANGED_LETTER_TEXT = 'Hi,\nYour mobile number has been changed and a new pin code sent to this number.\nHawkist'
+ADMIN_PHONE_NUMBER_CHANGED_LETTER_TEXT = 'Hi,\nYour mobile number has been changed and a new pin code sent to this ' \
+                                         'number.\nHawkist'
 ADMIN_PHONE_NUMBER_CHANGED_LETTER_SUBJECT = 'Mobile number changed'

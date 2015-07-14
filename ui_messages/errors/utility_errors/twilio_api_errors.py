dbe9eeb75dff6370563a1df000f116849d63eacf
Jonathan Erwin <jkeconsultancy@gmail.com>
2015-07-14 12:08:08 +0100

    Update twilio_api_errors.py

--- ui_messages/errors/utility_errors/twilio_api_errors.py
+++ ui_messages/errors/utility_errors/twilio_api_errors.py
@@ -1,10 +1,9 @@
 __author__ = 'ne_luboff'
 
-TWILIO_INVALID_PHONE_NUMBER = "Please input a mobile number starting in '07'. Or else Sign Up with a Facebook account."
-TWILIO_UNSUPPORTED_REGION = 'Permission to send an SMS has not been enabled for your region. Ask Twilio account owner ' \
-                            'add your country to supported list'
+TWILIO_INVALID_PHONE_NUMBER = 'Please input a mobile number starting in '07'. Or else Sign Up with a Facebook account.'
+TWILIO_UNSUPPORTED_REGION = 'Permission to send SMS has not been enabled for your region.'
 
 """
 Check number of sent sms last hour
 """
-TWILIO_RICH_SMS_LIMIT = 'You sent sms %s times per hour. Try again later'
+TWILIO_RICH_SMS_LIMIT = Too many SMS sent. Try again later.'

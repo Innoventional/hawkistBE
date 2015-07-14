a84287f2a6292a0d6b688657b924a2c0766920b2
Jonathan Erwin <jkeconsultancy@gmail.com>
2015-07-14 10:52:40 +0100

    Update admin_users_errors.py

--- ui_messages/errors/admin_errors/admin_users_errors.py
+++ ui_messages/errors/admin_errors/admin_users_errors.py
@@ -1,6 +1,6 @@
 __author__ = 'ne_luboff'
 
-ADMIN_UPDATE_USER_HAS_NOT_EMAIL = "Forbidden action.\nThis user has no email address in profile"
-ADMIN_UPDATE_USER_NOT_CONFIRM_EMAIL = "Forbidden action.\nThis user didn't confirm his email address yet"
+ADMIN_UPDATE_USER_HAS_NOT_EMAIL = "Forbidden action.\nThis user has no email address in his profile"
+ADMIN_UPDATE_USER_NOT_CONFIRM_EMAIL = "Forbidden action.\nThis user hasn't confirmed his email address yet"
 
 

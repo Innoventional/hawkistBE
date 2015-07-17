2812adf1a24216536471b9793fc5b88d3ea92d9f
ne-luboff <ne-luboff@yandex.ua>
2015-07-17 12:00:47 +0300

    Discount value cannot be less than 1 and more than 99

--- api/items/handlers.py
+++ api/items/handlers.py
@@ -852,8 +852,12 @@ class ListingHandler(ApiHandler):
         if selling_price != retail_price:
             # calculate discount value
             discount = int(round((retail_price - selling_price) / retail_price * 100))
+            # if discount less then 1 %
             if discount == 0:
                 discount = 1
+            # if discount almost equal to 100 %
+            if discount == 100:
+                discount = 99
             listing.discount = discount
 
         listing.shipping_price = shipping_price
